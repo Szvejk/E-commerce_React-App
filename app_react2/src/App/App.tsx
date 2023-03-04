0dd3846fd2505a292800d0c5868a6f2002666433
Sara Szwejkowska <saraszw@op.pl>
2023-03-04 19:00:32 +0100

    changed the nav section && created useState on menu

--- app_react2/src/App/App.tsx
+++ app_react2/src/App/App.tsx
@@ -3,13 +3,12 @@ import styles from './App.module.css';
 import Card from '../Card/Card';
 import Nav from '../Nav/Nav';
 
-
 const App = () => {
 	return (
-		<>
+		<div className={styles.wrapper}>
 			<Nav />
 			<Card />
-		</>
+		</div>
 	);
 };
 
--- app_react2/src/Nav/Nav.tsx
+++ app_react2/src/Nav/Nav.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Nav.module.css';
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { SlMenu } from 'react-icons/sl';
+
+
 const Nav = () => {
+	const [openNav, setOpenNav] = useState(false);
 	return (
 		<div className={styles.header}>
+			<div className={styles.mobile}>
+				<div className={styles.dropdownWrapper}>
+					<SlMenu
+						className={styles.menuMobileIcon}
+						onClick={() => setOpenNav((prev) => !prev)}
+					/>
+					{openNav && (
+						<div className={styles.dropdown}>
+							<ul className={styles.nav_mobile}>
+								<li>
+									<a href='#'>Collections</a>
+								</li>
+								<li>
+									<a href='#'>Men</a>
+								</li>
+								<li>
+									<a href='#'>Women</a>
+								</li>
+								<li>
+									<a href='#'>About</a>
+								</li>
+								<li>
+									<a href='#'>Contact</a>
+								</li>
+							</ul>
+						</div>
+					)}
+				</div>
+			</div>
 			<img className={styles.logo} src='./images/logo.svg' alt='' />
 			<nav>
 				<ul className={styles.nav_links}>
@@ -25,11 +58,15 @@ const Nav = () => {
 				</ul>
 			</nav>
 			<div className={styles.shoppingCard}>
-			<AiOutlineShoppingCart className={styles.shoppCard}/>
+				<AiOutlineShoppingCart className={styles.shoppCard} />
 			</div>
 
 			<div className={styles.avatar}>
-				<img className={styles.avatarImg} src='./images/image-avatar.png' alt='' />
+				<img
+					className={styles.avatarImg}
+					src='./images/image-avatar.png'
+					alt=''
+				/>
 			</div>
 		</div>
 	);
